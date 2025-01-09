32b2b1583ae42e3417511f4ee08eb454f646ab40
adityamishra139 <aditya2vibha@gmail.com>
2025-01-09 22:40:05 +0530

    folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import React from "react"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Testimonials from "./pages/Testimonials";
 
 function App() {
-  
-
   return (
-    <>
-     <div className="text-center">hello
-      
-     </div>
-    </>
-  )
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
