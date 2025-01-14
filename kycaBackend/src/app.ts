f53ea09fff481504a7cbd1ba48a2cedf26f2c117
Aditya-1708 <umeshaditya72@gmail.com>
2025-01-14 14:41:32 +0530

    added inquiry

--- kycaBackend/src/app.ts
+++ kycaBackend/src/app.ts
@@ -1,32 +1,20 @@
 import express, { Application } from 'express';
 import { PrismaClient } from '@prisma/client';
 import inquiryRoutes from './routes/inquiry';
-import heroRoutes from './routes/hero';
-
-// Initialize Express app
 const app: Application = express();
-const prisma = new PrismaClient(); // Initialize Prisma Client
-
-// Middleware to parse JSON
+const prisma = new PrismaClient();
 app.use(express.json());
-
-// Define routes
-app.use('/api/inquiries', inquiryRoutes);
-app.use('/api/hero', heroRoutes);
-
-// Start Server
+app.use('/api/inquiries',inquiryRoutes);
 app.listen(3000, async () => {
   try {
-    await prisma.$connect(); // Ensure Prisma is connected to the database
+    await prisma.$connect(); 
     console.log('Prisma connected to database');
     console.log('Server running on http://localhost:3000');
   } catch (error) {
     console.error('Error connecting to database:', error);
-    process.exit(1); // Exit if the database connection fails
+    process.exit(1);
   }
 });
-
-// Graceful shutdown
 process.on('SIGINT', async () => {
   await prisma.$disconnect();
   console.log('Prisma disconnected');
